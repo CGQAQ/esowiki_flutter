4c7fdf5eaa70c0a4590c4d62454230eb3c304b8b
Jason <m.jason.liu@outlook.com>
2022-12-03 21:56:31 +0800

    don't run on web target

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'dart:io' show Platform;
 import 'package:esomap_mobile/cupertino_app.dart';
 import 'package:esomap_mobile/material_app.dart';
+import 'package:flutter/foundation.dart';
 
 void main() {
+  if (kIsWeb) {
+    throw UnsupportedError('Web is not supported, Please use esomap instead');
+  }
+
   if (Platform.isAndroid ||
       Platform.isFuchsia ||
       Platform.isWindows ||
